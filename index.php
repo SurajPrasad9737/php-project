fa8a045f3377e41d0ddf760f29eac6cd4a450931
anil sidhu <webanilsidhu@gmail.com>
2021-02-27 00:00:42 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP Basic Project</title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <h1>Project Setup </h1>
+</body>
+</html>
